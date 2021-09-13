612db644b60905370a6fc131320c7bea8af7aacc
kylo2001 <kylo2001@naver.com>
2021-09-13 16:51:02 +0900

    Completed problem 8

---
+++ PART 1/Section02/remainder_operator.c
@@ -0,0 +1,40 @@
+/*
+    [문제 1]
+    정수형 데이터를 10진수로 입력 받아서 해당 데이터의 2진수 표현을 출력하는 프로그램을 구현해보자.
+    배열을 쓰지 않고 구현해보자.
+
+    [실행의 예]
+    10진수 정수 입력 : 12
+    12를 2진수로 변환하면 : 1100
+*/
+
+#include <stdio.h>
+
+int decimal_to_Binary(int decimal)
+{
+    int binary = 0;
+
+    for (int i = 0; decimal > 0; i++)
+    {
+        int digit = 1;
+        for (int j = i; j > 0; j--)
+            digit *= 10;
+
+        binary += (decimal % 2) * digit;
+        decimal /= 2;
+    }
+
+    return binary;
+}
+
+int main(void)
+{
+    int num;
+
+    fputs("10진수 정수 입력 : ", stdout);
+    scanf("%d", &num);
+
+    printf("%d를 2진수로 변환하면 : %d\n", num, decimal_to_Binary(num));
+
+    return 0;
+}
