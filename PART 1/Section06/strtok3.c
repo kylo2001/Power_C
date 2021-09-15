5bc3c1b9127d9188e95073573d9b5eb6e11a9f9d
kylo2001 <kylo2001@naver.com>
2021-09-15 10:36:32 +0900

    Modified problem 24

--- PART 1/Section06/strtok3.c
+++ PART 1/Section06/strtok3.c
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+    ! 내가 구현한 함수
 char *StringTokenize(char *str, char *delim)
 {
     int i = 1;
@@ -62,6 +64,54 @@ char *StringTokenize(char *str, char *delim)
         token_count = 1;
         return tokens[0];
     }
+}
+
+*/
+
+char *StringTokenize(char *str, char *delim)
+{
+    static char *nextTokenPos; // 정적 변수
+    char *curTokenPos;         // 현재 토큰 위치
+    char *curStrPos;           // 현재 문자열의 위치
+
+    if (str != NULL)
+    {
+        curTokenPos = str;         // 첫 번째 토큰 초기화를 해준다.
+        curStrPos = str;           // 문자열 위치 초기화
+        nextTokenPos = (char *)-1; // 이 변수는에는 문자열의 끝에 도달하면 NULL이 저장되는데 이와 구분 짓기 위해서 -1로 초기화 하였다.
+    }
+    else
+    { //다음 번 토큰을 얻기 위해서 함수가 다시 호출되었을 때 실행되는 영역
+        curTokenPos = nextTokenPos;
+        curStrPos = nextTokenPos;
+    }
+
+    // 모든 토큰이 반환되었을때는 NULL을 반환한다.
+    if (nextTokenPos == NULL)
+        return NULL;
+
+    // 토큰을 찾기 위해서 문자를 하나씩 비교하는 과정
+    while (1)
+    {
+        // 문자열의 끝에 도달했는지 제일 먼저 검사
+        if (*curStrPos == 0)
+        {
+            nextTokenPos = NULL;
+            break;
+        }
+
+        // 문자열의 끝이 아니라면, 토큰 구분문자인지 검사
+        if (*curStrPos == delim[0])
+        {
+            *curStrPos = 0;
+            nextTokenPos = curStrPos + 1;
+            break;
+        }
+
+        // 문자열의 끝이 아니고, 토큰 구분 문자가 아니라면 문자열 위치를 하나 증가시킨다.
+        curStrPos++;
+    }
+    return curTokenPos;
 }
 
 int main(void)
